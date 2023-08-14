ed5d945ea3316d0edc7858444de204a687e177f9
Magnus <magnusnguyen7@gmail.com>
2023-08-14 16:16:52 +0700

    5th commit

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,25 +1,56 @@
 import React from "react";
 import { connect } from 'react-redux'; 
-import { useNavigate } from "react-router-dom";
 import { logout } from "../actions/auth"; 
+import Button from '@mui/material/Button';
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
 
 const Dashboard = ({ logout }) => { 
 
-    const navigate = useNavigate();
+    const [anchorEl, setAnchorEl] = React.useState(null);
+    const open = Boolean(anchorEl);
   
+    const handleClick = (event) => {
+      setAnchorEl(event.currentTarget);
+    };
+  
+    const handleClose = () => {
+      setAnchorEl(null);
+    };
+
     const handleLogout = () => {
       logout();
-      navigate("/");
+      handleClose();
     };
   
     return (
       <div>
-        <h2>Welcome to the Dashboard</h2>
-        <button onClick={handleLogout}>Logout</button>
+        <Button
+          id="dashboard-menu-button"
+          aria-controls={open ? 'dashboard-menu' : undefined}
+          aria-haspopup="true"
+          aria-expanded={open ? 'true' : undefined}
+          onClick={handleClick}
+        >
+          Dashboard
+        </Button>
+        <Menu
+          id="dashboard-menu"
+          anchorEl={anchorEl}
+          open={open}
+          onClose={handleClose}
+          MenuListProps={{
+            'aria-labelledby': 'dashboard-menu-button',
+          }}
+        >
+          <MenuItem onClick={handleClose}>Profile</MenuItem>
+          <MenuItem onClick={handleClose}>My account</MenuItem>
+          <MenuItem onClick={handleLogout}>Logout</MenuItem>
+        </Menu>
       </div>
     );
 };
-  
+
 const mapActionsToProps = { logout }; 
 
 export default connect(null, mapActionsToProps)(Dashboard);
